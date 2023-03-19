f9b3d75c0d3827d70b24514c1c744537bd838409
Manuquim <manuelcabreraosorio11@gmail.com>
2023-03-19 21:50:51 +0000

    second commit

--- src/api/routes.py
+++ src/api/routes.py
@@ -5,7 +5,42 @@ from flask import Flask, request, jsonify, url_for, Blueprint
 from api.models import db, User
 from api.utils import generate_sitemap, APIException
 
+from flask_jwt_extended import create_access_token
+from flask_jwt_extended import get_jwt_identity
+from flask_jwt_extended import jwt_required
+
 api = Blueprint('api', __name__)
+
+# Create a route to authenticate your users and return JWTs. The
+# create_access_token() function is used to actually generate the JWT.
+@api.route("/login", methods=["POST"])
+def login():
+    email = request.json.get("email", None)
+    password = request.json.get("password", None)
+    #realizar la logica de validacion
+    if email != "test" or password != "test":
+        return jsonify({"msg": "Bad email or password"}), 401
+
+    access_token = create_access_token(identity=email)
+    response_body={
+        "email":email,
+        "access_token":access_token
+    }
+    return jsonify(response_body)
+
+
+# Protect a route with jwt_required, which will kick out requests
+# without a valid JWT present.
+@api.route("/private", methods=["GET"])
+@jwt_required()
+def protected():
+    # Access the identity of the current user with get_jwt_identity
+    current_user = get_jwt_identity()
+    response_body={
+        "email":current_user,
+        "message":"logged_in"
+    }
+    return jsonify(response_body), 200
 
 
 @api.route('/hello', methods=['POST', 'GET'])
